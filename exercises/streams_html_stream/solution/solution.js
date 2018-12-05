03268e4cdcee66fb3103a9129a82ac1bf212448a
Christophe Porteneuve <tdd@tddsworld.com>
2018-12-05 08:55:49 +0100

    refactor: trumpet solution now demoes all-matches variant

--- exercises/streams_html_stream/solution/solution.js
+++ exercises/streams_html_stream/solution/solution.js
@@ -5,11 +5,22 @@ const tr = trumpet()
 
 process.stdin.pipe(tr).pipe(process.stdout)
 
+// Variante « première occurrence seulement » :
+
 const hook = tr.select('.loud').createStream()
-const upperCaser = new Transform({
+hook.pipe(upperCaser()).pipe(hook)
+
+// Variante « toutes les occurrences » :
+
+tr.selectAll('.loud', (element) => {
+  const hook = element.createStream()
+  hook.pipe(upperCaser()).pipe(hook)
+})
+
+// L’utilitaire de création de flux de transfo majuscule :
+
+const upperCaser = () => new Transform({
   transform(buf, enc, cb) {
     cb(null, buf.toString().toUpperCase())
   },
 })
-
-hook.pipe(upperCaser).pipe(hook)
